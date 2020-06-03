1fd8c8d8173846efb31ef80c928ae4ea266b47b7
pluiel <pluiel@outlook.com>
2020-06-03 22:45:08 +0900

    TableView

---
+++ Piechart/PiechartViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PiechartViewController.swift
+//  Piechart
+//
+//  Created by Yuka Uematsu on 2020/06/03.
+//  Copyright © 2020 Uematsu Well. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class PiechartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    
+
+}
--- Piechart/ViewController.swift
+++ Piechart/ViewController.swift
@@ -8,15 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController:
+    UIViewController, UITableViewDelegate,
+    UITableViewDataSource, UITextFieldDelegate{
+    
+    
+    @IBOutlet weak var kadai: UILabel!
+    @IBOutlet weak var time: UILabel!
+    
+    @IBOutlet weak var kadaitextField: UITextField!
+    @IBOutlet weak var timetextField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var kadaitextArray = [String]()
+    var timetextArray = [String]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-  
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        kadaitextField.delegate = self
+        timetextField.delegate = self
     }
-    @IBOutlet var namelabel: UILabel!
-    @IBOutlet var timelabel: UILabel!
+    
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        
+        return Int
 }
 
-
+   
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return Int
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell{
+            
+            let cell =
+                tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            
+            cell.textLabel?.text = kadaitextArray[indexPath.row]
+            
+            return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let nextVC =
+            storyboard?.instantiateViewController(identifier: "next") as!
+        NextViewController
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.size.height/12
+        
+    }
+    
+    func kadaitextFieldShouldReturn(_ textField: UITextField) -> Bool {
+        kadaitextArray.append(textField.text!)
+        kadaitextField.resignFirstResponder()
+        kadaitextField.text = ""
+        tableView.reloadData()
+        
+        return true
+    }
+    
+}
